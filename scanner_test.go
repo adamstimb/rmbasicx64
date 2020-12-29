d99922277f215a5df0f4315c3ce781f2f7b9ca28
Tim Adams <adamstimb@gmail.com>
2020-12-29 08:46:40 +0100

    scanner tests working

--- scanner_test.go
+++ scanner_test.go
@@ -14,18 +14,40 @@ func TestScanner(t *testing.T) {
 			Source: "print \"Hello!\"",
 			ExpectedTokens: []Token{
 				{PRINT, "PRINT"},
-				{StringLiteral, "Hello\""},
+				{StringLiteral, "Hello!"},
+			},
+		},
+		{
+			Source: "Rem This is a comment",
+			ExpectedTokens: []Token{
+				{REM, "REM"},
+				{Comment, "This is a comment"},
+			},
+		},
+		{
+			Source: "this% :=that$+ meh - 5.1234",
+			ExpectedTokens: []Token{
+				{Identifier, "This%"},
+				{Assign, ":="},
+				{Identifier, "That$"},
+				{Plus, "+"},
+				{Identifier, "Meh"},
+				{Minus, "-"},
+				{NumericalLiteral, "5.1234"},
 			},
 		},
 	}
 
-	// run tests
+	// test that we always get expected tokens
 	s := &Scanner{}
 	for i, test := range tests {
 		tokens := s.ScanTokens(test.Source)
 		for j, token := range tokens {
 			if token.TokenType != test.ExpectedTokens[j].TokenType {
-				t.Fatalf("Test [%d]: TokenType [%d] expected, got [%d] from source [%q]", i, test.ExpectedTokens[j].TokenType, token.TokenType, test.Source)
+				t.Fatalf("TestScanner [%d]: TokenType [%d] expected, got [%d] from source [%q]", i, test.ExpectedTokens[j].TokenType, token.TokenType, test.Source)
+			}
+			if token.Literal != test.ExpectedTokens[j].Literal {
+				t.Fatalf("TestScanner [%d]: Literal [%q] expected, got [%q] from source [%q]", i, test.ExpectedTokens[j].Literal, token.Literal, test.Source)
 			}
 		}
 	}
