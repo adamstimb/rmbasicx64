f5e03ee79e687e53ace1bde62abd6b744d4db403
Tim Adams <adamstimb@gmail.com>
2020-12-29 08:39:15 +0100

    writing tests

--- scanner_test.go
+++ scanner_test.go
@@ -1,5 +1,32 @@
+package main
+
 import "testing"
 
 func TestScanner(t *testing.T) {
-	
+
+	// test data
+	type test struct {
+		Source         string
+		ExpectedTokens []Token
+	}
+	tests := []test{
+		{
+			Source: "print \"Hello!\"",
+			ExpectedTokens: []Token{
+				{PRINT, "PRINT"},
+				{StringLiteral, "Hello\""},
+			},
+		},
+	}
+
+	// run tests
+	s := &Scanner{}
+	for i, test := range tests {
+		tokens := s.ScanTokens(test.Source)
+		for j, token := range tokens {
+			if token.TokenType != test.ExpectedTokens[j].TokenType {
+				t.Fatalf("Test [%d]: TokenType [%d] expected, got [%d] from source [%q]", i, test.ExpectedTokens[j].TokenType, token.TokenType, test.Source)
+			}
+		}
+	}
 }
