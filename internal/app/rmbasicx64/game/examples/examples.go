429e63e152c30684e2329fe73f50c53b49447a8c
adamstimb <adamstimb@gmail.com>
2022-04-15 13:24:18 +0200

    lathe

--- internal/app/rmbasicx64/game/examples/examples.go
+++ internal/app/rmbasicx64/game/examples/examples.go
@@ -335,6 +335,275 @@ func WriteExamples(workspacePath string) {
 590 IF Op% = 7 THEN Cut : GOTO 130
 600 IF Op% = 8 THEN Quit : END
 610 GOTO 450
+620 PROCEDURE Directory
+630   SET WRITING 0
+640   SET PAPER 10
+650   SET BORDER 10
+660   SET BRUSH 0
+670   CLG
+680   AREA 30, 0; 310, 0; 310, 230; 30, 240; 30, 0 BRUSH 2
+690   AREA 30, 0; 310, 0; 310, 230; 30, 240; 30, 0 BRUSH 0 STYLE 0
+700   AREA 10, 20; 290, 20; 290, 249; 10, 249; 10, 20 BRUSH 15
+710   AREA 10, 20; 290, 20; 290, 249; 10, 249; 10, 20 BRUSH 0 STYLE 0
+720   PLOT "Shapes Available.", 40, 0 BRUSH 0 SIZE 2, 2
+730   PLOT "Shapes Available.", 41, 1 BRUSH 13 SIZE 2, 2
+740   SET WRITING 2 TO 3, 2; 36, 22
+750   SET WRITING 2
+760   SET PAPER 15 : SET PEN 0
+770   DIR "*.3dt"
+780   ASK MOUSE X%, Y%, B%
+790   IF B% = 0 THEN 780
+800 ENDPROC
+810 PROCEDURE Quit
+820   SET WRITING 0
+830   SET PAPER 8 : SET BORDER 8
+840   CLG
+850   PLOT "Have a nice day!", 35, 230 SIZE 2, 2 BRUSH 0
+860   PLOT "Have a nice day!", 36, 229 SIZE 2, 2 BRUSH 14
+870   PLOT "Thank you for using this program.", 30, 200 SIZE 1, 2 BRUSH 0
+880   PLOT "Thank you for using this program.", 31, 199 SIZE 1, 2 BRUSH 12
+890   PLOT "** GOODBYE **", 0, 100 SIZE 3, 3 BRUSH 15
+900   Of := 0
+910   FOR N := 0 TO 15
+920     LINE 60, 116 + N; 247, 166 + N BRUSH (N + Of) AND 15 OVER FALSE
+930     LINE 60, 115 - N; 247, 115 - N BRUSH (N + Of) AND 15 OVER FALSE
+940   NEXT
+950   Of := Of + 1 : IF Of = 16 THEN Of := 0
+960   ASK MOUSE X%, Y%, B%
+970   IF B% = 0 THEN 910
+980 ENDPROC
+990 PROCEDURE Ed
+1000   GLOBAL Ro(), Yo(), T%
+1010   SET PAPER 0 : SET BORDER 0 : CLG : SET WRITING 0 : HOME
+1020   SET POINTS STYLE 3
+1030   AREA 56, 38; 56, 238; 282, 238; 282, 38; 56, 38 BRUSH 8
+1040   AREA 56, 38; 56, 238; 282, 238; 282, 38; 56, 38 BRUSH 0 STYLE 0
+1050   FOR N := 46 TO 240 STEP 16
+1060     LINE 50, N; 280, N BRUSH 4
+1070   NEXT
+1080   FOR N := 50 TO 280 STEP 16
+1090	 LINE N, 46; N, 240 BRUSH 4
+1100   NEXT
+1110   Ro(1) := 0 : Yo(1) := 50
+1120   Cx := 160 : Cv := 125
+1130   Px := Cx + Po(1) : Py := Cy - Yo(1)
+1140   Xpos% := Px : Ypos% := Py : Xposn% := Px : Yposn% := Py
+1150   T% := 1
+1160   Mx := Px : My := Py : Pxm := Px : Pym := Py
+1170   LINE Pxm, Pym; Mx, My BRUSH 15 OVER FALSE
+1180   LINE Px, Py; Xpos%, Ypos% BRUSH 15 OVER FALSE
+1190   Mx := Cx + (-1 * (Xposn% - Cx))
+1200   My := Cy - ((Yposn% - Cy))
+1210   POINTS Xpos%, Ypos% BRUSH 2 OVER FALSE
+1220   ASK MOUSE Xposn%, Yposn%, Button%
+1230   IF Button% = 2 AND Xposn% > 160 THEN 1340
+1240   IF Button% = 3 THEN 1430
+1250   IF Xposn% = Xpos% AND Yposn% = Ypos% THEN 1220
+1260   IF Xposn% < 160 THEN 1220
+1270   POINTS Xpos%, Ypos% BRUSH 2 OVER FALSE
+1280   LINE Px, Py; Xpos%, Ypos% BRUSH 15 OVER FALSE
+1290   LINE Pxm, Pym; Mx, My BRUSH 15 OVER FALSE
+1300   Xpos% := Xposn% : Ypos% := Yposn%
+1310   Mx := Cx + (-1 * (Xposn% - Cx))
+1320   My := Cy - (Yposn% - Cy)
+1330   GOTO 1170
+1340   Px := Xposn% : Py := Yposn%
+1350   Pxm := Cx + (-1 * (Xposn% - Cx))
+1360   Pym := Cy - (Yposn% - Cy)
+1370   Yo(T%) := Yposn% - Cv
+1380   Ro(T%) := Xposn% - Cx
+1390   T% := T% + 1
+1400   IF T% = 50 THEN 1430
+1410   FOR P := 0 TO 500 : NEXT
+1420   GOTO 1170
+1430 ENDPROC
+1440 PROCEDURE Savdat
+1450   GLOBAL Ro(), Yo(), T%, F$
+1460   SET PAPER 12 : SET BORDER 12 : CLG : SET WRITING 0 : CLS
+1470   AREA 50, 100; 270, 230; 50, 230; 50, 100 BRUSH 4
+1480   AREA 50, 100; 270, 230; 50, 230; 50, 100 BRUSH 0 STYLE 0
+1490   AREA 30, 120; 250, 120; 250, 250; 30, 250; 30, 120 BRUSH 15
+1500   AREA 30, 120; 250, 120; 250, 250; 30, 250; 30, 120 BRUSH 0 STYLE 0
+1510   SET WRITING 2 TO 5, 2; 31, 12
+1520   SET WRITING 2
+1530   SET PAPER 15
+1540   SET PEN 0
+1550   CLS
+1560   PLOT "Enter filename to be save. (8 Chars)", 10, 75 SIZE 1, 2 BRUSH 0
+1570   PLOT "Enter filename to be save. (8 Chars)", 11, 74 SIZE 1, 2 BRUSH 9
+1580   HOME : INPUT F%
+1590   IF LEN F$  > 8 OR LEN F$ = 0 THEN 1730 : REM Uh oh!
+1600   F$ := F$ + ".3dt"
+1610   PLOT "Please Wait...", 91, 199 SIZE 1, 2 BRUSH 0
+1620   PLOT "Please Wait...", 90, 200 SIZE 1, 2 BRUSH 8
+1630   P$ := "Saving:- " + F$
+1640   PLOT P$, 70 + ((8 - LEN F$) * 4), 170 SIZE 1, 2 BRUSH 0
+1650   PLOT P$, 71 + ((8 - LEN F$) * 4), 169 SIZE 1, 2 BRUSH 2
+1660   CREATE #11, F$
+1670   PRINT #11, T%
+1680   FOR D% := 1 TO T%
+1690     PRINT #11, Yo(D%), Ro(D%)
+1700   NEXT
+1710   CLOSE #11
+1720 ENDPROC
+1730 CLS : GOTO 1580 : REM Yikes...
+1740 PROCEDURE Loaddat
+1750   GLOBAL Ro(), Yo(), T%, F$
+1760   SET PAPER 12 : SET BORDER 12 : CLG : SET WRITING 0 : CLS
+1770   AREA 50, 100; 270, 100; 270, 230; 50, 230; 50, 100 BRUSH 4
+1780   AREA 50, 100; 270, 100; 270, 230; 50, 230; 50, 100 BRUSH 0 STYLE 0
+1790   AREA 30, 120; 250, 120; 250, 250; 30, 250; 30, 120 BRUSH 15
+1800   AREA 30, 120; 250, 120; 250, 250; 30, 250; 30, 120 BRUSH 0 STYLE 0
+1810   SET WRITING 2 TO 5, 2; 31, 12
+1820   SET WRITING 2
+1830   SET PAPER 15
+1840   SET PEN 0
+1850   CLS
+1860   PLOT "Enter filename to be loaded. (8 Chars)", 10, 75 SIZE 1, 2 BRUSH 0
+1870   PLOT "Enter filename to be loaded. (8 Chars)", 11, 74 SIZE 1, 2 BRUSH 9
+1880   HOME : INPUT F$
+1890   IF LEN F$ > 8 OR LEN F$ = 0 THEN 1880
+1900   F$ := F$ + ".3dt"
+1910   PLOT "Please Wait...", 91, 199 SIZE 1, 2 BRUSH 0
+1920   PLOT "Please Wait...", 90, 200 SIZE 1, 2 BRUSH 8
+1930   P$ := "Loading:- " + F$
+1940   PLOT P$, 70 + ((8 - LEN F$) * 4), 170 SIZE 1, 2 BRUSH 0
+1950   PLOT P$, 71 + ((8 - LEN F$) * 4), 169 SIZE 1, 2 BRUSH 2
+1960   OPEN #11, F$
+1970   INPUT #11, T%
+1980   FOR D% := 1 TO T%
+1990     INPUT #11, Yo(D%), Ro(D%)
+2000   NEXT
+2010   CLOSE #11
+2020 ENDPROC
+2030 PROCEDURE View
+2040   GLOBAL Ro(), Yo(), T%, F$
+2050   SET MODE 40
+2060   SET DEG TRUE
+2070   SET PAPER 2 : CLG
+2080   SET WRITING 3 TO 2, 2; 38, 3
+2090   SET WRITING 3
+2100   AREA 10, 239; 319, 239; 319, 200; 100, 200 BRUSH 8
+2110   AREA 10, 239; 319, 239; 319, 200; 100, 200 BRUSH 0 STYLE 0
+2120   SET PAPER 15 : SET PEN 0 : SET BRUSH 136
+2130   AREA 0, 249; 309, 249; 309, 210; 0, 210 BRUSH 15
+2140   AREA 0, 249; 309, 249; 309, 210; 0, 210 BRUSH 0 STYLE 0
+2150   CLS
+2160   SET DRAWING 1 TO 0, 0; 319, 199
+2170   SET DRAWING 1
+2180   Angle RECEIVE Ang
+2190   A := Ang
+2200   SET PAPER 2 : CLG : SET PAPER 15
+2210   S := SIN A : C := COS 30
+2220   B := 1 : Cx := 160 : Cy := 75 : Dist := 100 : D := 200
+2230   Mousey RECEIVE Cx, Cy
+2240   SET PATTERN 136, 1 TO 0, 8, 0, 8
+2250   SET PATTERN 136, 2 TO 8, 0, 8, 0
+2260   SET PATTERN 136, 3 TO 0, 8, 0, 8
+2270   SET PATTERN 136, 4 TO 8, 0, 8, 0
+2280   SET PATTERN 137, 1 TO 8, 7, 8, 7
+2290   SET PATTERN 137, 2 TO 7, 8, 7, 8
+2300   SET PATTERN 137, 3 TO 8, 7, 8, 7
+2310   SET PATTERN 137, 4 TO 7, 8, 7, 8
+2320   SET PATTERN 138, 1 TO 15, 7, 15, 7
+2330   SET PATTERN 138, 2 TO 7, 15, 7, 15
+2340   SET PATTERN 138, 3 TO 15, 7, 15, 7
+2350   SET PATTERN 138, 4 TO 7, 15, 7, 15
+2360   FOR N := 1 TO T% - 2
+2370     B := 0
+2380     R1 := Ro(N) : R2 := Ro(N + 1) : Y1 := Yo(N) : Y2 := Yo(N + 1) GOSUB 2440
+2390   NEXT
+2400   ASK MOUSE X%, Y%, B%
+2410   IF B% = 0 THEN 2400
+2420   SET DRAWING 0
+2430 ENDPROC
+2440 FOR A := 135 - 185 - 180 TO 105 STEP 30
+2450   X := R1 * SIN A
+2460   Z := R1 * COS A
+2470   Px1 := ((X - Z) * C) + Cx
+2480   Py1 := (Y1 - (X + Z) * S) + Cy
+2490   X := R2 * SIN A
+2500   Z := R2 * COS A
+2510   Px2 := ((X - Z) * C) + Cx
+2520   Py2 := (Y2 - (X + Z) * S) + Cy
+2530   X := R2 * SIN(A + 30)
+2540   Z := R2 * COS(A + 30)
+2550   Px3 := ((X - Z) * C) + Cx
+2560   Py3 := (Y2 - (X + Z) * S) + Cy
+2570   X := R1 * SIN(A + 30)
+2580   Z := R1 * COS(A + 30)
+2590   Px4 := ((X - Z) * C) + Cx
+2600   Py4 := (Y1 - (X + Z) * S) + Cy
+2610   AREA Px1, Py1; Px2, Py2; Px3, Py3; Px4, Py4
+2620   AREA Px1, Py1; Px2, Py2; Px3, Py3; Px4, Py4 STYLE 3 BRUSH 15
+2630   B := B + 1
+2640   IF B = 1 THEN SET BRUSH 0
+2650   IF B = 2 OR B = 12 THEN SET BRUSH 136
+2660   IF B = 3 OR B = 11 THEN SET BRUSH 8
+2670   IF B = 4 OR B = 10 THEN SET BRUSH 137
+2680   IF B = 5 OR B = 9 THEN SET BRUSH 7
+2690   IF B = 7 THEN SET BRUSH 15
+2700   IF B = 8 OR B = 6 THEN SET BRUSH 138
+2710 NEXT
+2720 RETURN
+2730 PROCEDURE Mousey RETURN Cx, Cy
+2740   PRINT "Select centre of shape with mouser."
+2750   SET MOUSE
+2760   SET POINTS STYLE 2
+2770   ASK MOUSE Cx%, Cy%, B%
+2780   POINTS Cx, Cy OVER FALSE
+2790   POINTS Cx, Cy OVER FALSE
+2800 IF B% = 2 THEN ENDPROC
+2810 GOTO 2770
+2820 PROCEDURE Plott
+2830   CREATE #15, "ltp1"
+2840   GLOBAL T%, F$, Yo(), Ro()
+2850   SET DEG TRUE
+2860   SET PAPER 1 : SET BORDER 1 : CLG
+2870   PLOT "Plot Drawing", 55, 220 SIZE 2, 2 BRUSH 0
+2880   PLOT "Plot Drawing", 56, 219 SIZE 2, 2 BRUSH 2
+2890   Angle RECEIVE Ang
+2900   IF Ang = 0 THEN Sta := - 50
+2910   IF Ang = 90 THEN Sta := - 230
+2920   FOR N := 1 TO T% - 2
+2930     Ox := 700 : Ov := 700 : Scx := 6 : Scv = 5 : R1 := Ro(N) : R2 := Ro(N + 1) : Y1 := Yo(N) : Y2 := Yo(N + 1) : GOSUB 3010
+2940     X := Ro(N + 1) * SIN A
+2950     Z := Ro(N + 1) * COS A
+2960     PRINT #15, "M "; Ox - LEN F$ * 4; " "; Scv * Yo(0) - Ov
+2970     PRINT #15, "P Drawing := ": F$
+3000 CLOSE #15 : ENDPROC
+3010 Cv := 400 : Cx := 400 : S := SIN Ang : C := COS 30
+3020 FOR A := Sta TO 105 STEP 30
+3030   X := R1 * SIN A
+3040   Z := R1 * COS A
+3050   Px1 := ((X - Z) * C) + Cx
+3060   Py1 := (Y1 - (X + Z) * S) + Cy
+3070   X := R2 * SIN A
+3080   Z := R2 * COS A
+3090   Px2 := ((X - Z) * C) + Cx
+3100   Py2 := (Y2 - (X + Z) * S) + Cy
+3110   X := R2 * SIN(A + 30)
+3120   Z := R2 * COS(A + 30)
+3130   Px3 := ((X - Z) * C) + Cx
+3140   Py3 := (Y2 - (X + Z) * S) + Cy
+3150   X := R1 * SIN(A + 30)
+3160   Z := R1 * COS(A + 30)
+3170   Px4 := ((X - Z) * C) + Cx
+3180   Py4 := (Y1 - (X + Z) * S) + Cy
+3190   PRINT #15, "M "; (Px1 * Scx) - Ox; " "; (Py1 * Scv) - Ov;
+3200   PRINT #15, "D "; (Px1 * Scx) - Ox; " "; (Py1 * Scv) - Ov; " "; (Px2 * Scx) - Ox; " "; (Py2 * Scv) - Ov;
+3210   PRINT #15, "D "; (Px2 * Scx) - Ox; " "; (Py2 * Scv) - Ov; " "; (Px3 * Scx) - Ox; " "; (Py3 * Scv) - Ov;
+3220   PRINT #15, "D "; (Px3 * Scx) - Ox; " "; (Py3 * Scv) - Ov; " "; (Px4 * Scx) - Ox; " "; (Py4 * Scv) - Ov;
+3230   PRINT #15, "D "; (Px4 * Scx) - Ox; " "; (Py4 * Scv) - Ov; " "; (Px1 * Scx) - Ox; " "; (Py1 * Scv) - Ov;
+3240 NEXT
+3250 RETURN
+
+
+
+
+
+
+
 `,
 		},
 	}
